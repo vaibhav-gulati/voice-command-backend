b579f1717eabd945c614886665bf012fe2c2059e
vaibhav-gulati <vaibhavgulati750@gmail.com>
2023-10-02 17:18:27 +0530

    box backend working

--- app.js
+++ app.js
@@ -1,66 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const nlp = require('compromise');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
-
-// Enable CORS for all routes
 app.use(cors());
 
-// Function to parse voice commands and return actionable commands
+const validColors = [
+  'aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond',
+  'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue',
+  'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkkhaki',
+  'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue',
+  'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick',
+  'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow',
+  'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon',
+  'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightpink', 'lightsalmon',
+  'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen',
+  'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen',
+  'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose',
+  'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod',
+  'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple',
+  'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna',
+  'silver', 'skyblue', 'slateblue', 'slategray', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato',
+  'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'
+];
+
+
 function parseVoiceCommand(audio) {
   try {
-    // Use compromise to process the voice command and extract meaningful information
-    const doc = nlp(audio);
-
-    // Implement logic to classify and extract actionable commands based on parsed data
-    const actionableCommand = classifyVoiceCommand(doc);
-
-    return actionableCommand;
+    const words = audio.toLowerCase().split(' ');
+    const color = words.find(word => validColors.includes(word));
+    if (color) {
+      return `addBox("${color}")`;
+    } else {
+      return 'unrecognizedCommand()';
+    }
   } catch (error) {
     console.error('Error parsing voice command:', error);
     return null;
   }
 }
 
-// Function to classify and extract actionable commands
-function classifyVoiceCommand(doc) {
-  // Implement your logic here to classify and extract actionable commands
-  // For this example, we'll use a simplified mock
-  const text = doc.out('text').toLowerCase();
-
-  if (text.includes('add') && text.includes('blue box')) {
-    return 'addBox("blue")';
-  } else if (text.includes('switch') && text.includes('dark mode')) {
-    return 'switchToDarkMode()';
-  } else {
-    return 'unrecognizedCommand()';
-  }
-}
-
 app.post('/voice-command', (req, res) => {
   const { audio } = req.body;
 
-  // Parse the voice command
   const actionableCommand = parseVoiceCommand(audio);
 
-  // Execute the parsed actionable command (this is a mock for demonstration)
   const response = executeActionableCommand(actionableCommand);
 
   res.json(response);
 });
 
-// Mock function to execute the parsed actionable command (customize this for your app)
 function executeActionableCommand(actionableCommand) {
   try {
-    // In a real application, you would execute the corresponding actions here
-    // For this example, we'll return a simple success message
     console.log('Executing actionable command:', actionableCommand);
-    return { success: true, message: 'Command executed successfully' };
+    return { success: true, command: actionableCommand };
   } catch (error) {
     console.error('Error executing command:', error);
     return { success: false, error: 'Error executing command' };
